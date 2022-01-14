c72717272514c48e2704dda3959896d36ad118b4
Gregtmlg <greg.glemot@gmail.com>
2022-01-14 10:50:26 +0100

    allumage robot

--- Robot.py
+++ Robot.py
@@ -6,5 +6,14 @@ class Robot():
             self.__name = name
         else :
             self.__name = "<unnamed>"
+        self.__state = False
+
+    #allumage robot
+    def power_on(self):
+        if self.__state:
+            return """Robot already ON"""
+        else :
+            self.__state = True
+            return """Robot ignition"""
     
 
