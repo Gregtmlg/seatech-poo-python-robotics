ada3d21c53386a2703c9584345ed5a6cb53da204
Gregtmlg <greg.glemot@gmail.com>
2022-01-14 11:14:44 +0100

    mise en dossier exo_1

---
+++ exo_1/Robot.py
@@ -0,0 +1,19 @@
+import time
+
+class Robot():
+    def __init__(self, name):
+        if name:
+            self.__name = name
+        else :
+            self.__name = "<unnamed>"
+        self.__state = False
+
+    #allumage robot
+    def power_on(self):
+        if self.__state:
+            return """Robot already ON"""
+        else :
+            self.__state = True
+            return """Robot ignition"""
+    
+
