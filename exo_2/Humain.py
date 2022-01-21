579220630c6f4c8e1d3caa58c79274ebf5855587
Gregtmlg <greg.glemot@gmail.com>
2022-01-21 15:09:12 +0100

    Modification de la méthode eat dans Humain

--- exo_2/Humain.py
+++ exo_2/Humain.py
@@ -7,13 +7,14 @@ class Humain():
         self.__good_food = []
         self.__bad_food = ['cailloux']
 
-    def eat(self, aliment = 'cailloux'):
-        if aliment in self.__good_food:
-            print(f"""{self.__name} aime bien cet aliment : {aliment}""")
-        elif aliment in self.__bad_food:
-            print(f"""{self.__name} n'aime pas cet aliment : {aliment}""")
-        else:
-            self.__like_or_not(aliment)
+    def eat(self, aliment_list = ['cailloux']):
+        for aliment in aliment_list:
+            if aliment in self.__good_food:
+                print(f"""{self.__name} aime bien cet aliment : {aliment}""")
+            elif aliment in self.__bad_food:
+                print(f"""{self.__name} n'aime pas cet aliment : {aliment}""")
+            else:
+                self.__like_or_not(aliment)
 
     def __like_or_not(self, aliment):
         choix = rd.randint(0,1)
