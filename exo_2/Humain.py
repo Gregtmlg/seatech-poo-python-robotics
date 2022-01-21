a06c81ff86bb31f1eb711a16cea6766ef6824a97
Gregtmlg <greg.glemot@gmail.com>
2022-01-21 15:01:21 +0100

    Etapes  2 et 3 validées

--- exo_2/Humain.py
+++ exo_2/Humain.py
@@ -1,2 +1,25 @@
+import random as rd
+
 class Humain():
-    pass
+    def __init__(self, name = '<unnamed>', sexe = '<No Gender>'):
+        self.__name = name
+        self.__sexe = sexe
+        self.__good_food = []
+        self.__bad_food = ['cailloux']
+
+    def eat(self, aliment = 'cailloux'):
+        if aliment in self.__good_food:
+            print(f"""{self.__name} aime bien cet aliment : {aliment}""")
+        elif aliment in self.__bad_food:
+            print(f"""{self.__name} n'aime pas cet aliment : {aliment}""")
+        else:
+            self.__like_or_not(aliment)
+
+    def __like_or_not(self, aliment):
+        choix = rd.randint(0,1)
+        if choix == 1:
+            self.__good_food.append(aliment)
+            print(f"""{self.__name} aime bien cet aliment : {aliment}""")
+        else : 
+            self.__bad_food.append(aliment)
+            print(f"""{self.__name} n'aime pas cet aliment : {aliment}""")
